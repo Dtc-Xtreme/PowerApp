9045a6ce82a0defa7778333d90e4e7d7a9407b67
dtc-xtreme <dtc-xtreme@hotmail.com>
2022-11-18 18:46:25 +0100

    remove of other app

--- ScatterChartGetStarted/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScatterChartGetStarted;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- ScatterChartGetStarted/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ScatterChartGetStarted;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
