cd24d5aaac7fc3a83ddc34474d0427bac24cce03
dtc-xtreme <dtc-xtreme@hotmail.com>
2022-11-23 15:58:10 +0100

    rework

--- PowerApp/ViewModels/HomeViewModel.cs
+++ PowerApp/ViewModels/HomeViewModel.cs
@@ -15,40 +15,6 @@ namespace PowerApp.ViewModels
         private VerbruikWaterViewModel waterVM;
         private VerbruikGasViewModel gasVM;
 
-        //private int totalKwh;
-        //private int totalLiters;
-
-        //public int TotalKwh
-        //{
-        //    get { return totalKwh; }
-        //    set
-        //    {
-        //        SetProperty(ref totalKwh, value);
-        //        CheckSmileyForKwh();
-        //    }
-        //}
-        //public int TotalLiters
-        //{
-        //    get { return totalLiters; }
-        //    set
-        //    {
-        //        SetProperty(ref totalLiters, value);
-        //        CheckSmileyForLiters();
-        //    }
-        //}
-
-        //[ObservableProperty]
-        //private bool isSmilingKwh;
-
-        //[ObservableProperty]
-        //private bool isSmilingLiters;
-
-        //[ObservableProperty]
-        //private Color backgroundColorKwh;
-
-        //[ObservableProperty]
-        //private Color backgroundColorLiters;
-
         [ObservableProperty]
         private Electriciteit selectedElectricity;
 
@@ -72,47 +38,9 @@ namespace PowerApp.ViewModels
             SelectedGas = gasVM.List.LastOrDefault(c => c.Kwh != 0);
         }
 
-        //private void CheckSmileyForKwh()
-        //{
-        //    if (totalKwh <= 2479)
-        //    {
-        //        IsSmilingKwh = true;
-        //        BackgroundColorKwh = Color.Parse("#1ed822");
-        //    }
-        //    else
-        //    {
-        //        IsSmilingKwh = false;
-        //        BackgroundColorKwh = Color.Parse("#d02a22");
-        //    }
-        //}
-        //private void CheckSmileyForLiters()
-        //{
-        //    if (TotalLiters <= 2479)
-        //    {
-        //        IsSmilingLiters = true;
-        //        BackgroundColorLiters = Color.Parse("#1ed822");
-        //    }
-        //    else
-        //    {
-        //        IsSmilingLiters = false;
-        //        BackgroundColorLiters = Color.Parse("#d02a22");
-        //    }
-        //}
-
-        //[RelayCommand]
-        //private void Toggle()
-        //{
-        //    if(TotalKwh == 2453)
-        //    {
-        //        TotalKwh = 3900;
-        //        TotalLiters = 2174;
-        //    }
-        //    else
-        //    {
-        //        TotalKwh = 2453;
-        //        TotalLiters = 4850;
-        //    }
-
-        //}
+        [RelayCommand]
+        private void Toggle()
+        {
+        }
     }
 }
